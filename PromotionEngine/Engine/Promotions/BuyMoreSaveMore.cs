e45d3ecd21592bb9a1af5c664fae35f6a7bd8f12
Chaitanya <chaitanya.net2020@gmail.com>
2020-08-05 10:54:24 +0530

    adding promotion properties

--- PromotionEngine/Engine/Promotions/BuyMoreSaveMore.cs
+++ PromotionEngine/Engine/Promotions/BuyMoreSaveMore.cs
@@ -6,6 +6,17 @@ namespace Engine.Promotions
 {
     public class BuyMoreSaveMore : IPromotion
     {
+        public char PromotedProductSku { get; private set; }
+        public int PromotionQuantity { get; private set; }
+        public decimal PromotionAmount { get; private set; }
+
+        public BuyMoreSaveMore(char sku, int quantity, decimal promotionAmount)
+        {
+            PromotedProductSku = sku;
+            PromotionQuantity = quantity;
+            PromotionAmount = promotionAmount;
+        }
+
         public Cart Calculate(Cart cart)
         {
             throw new NotImplementedException();
--- PromotionEngine/Engine/Promotions/ComboOffer.cs
+++ PromotionEngine/Engine/Promotions/ComboOffer.cs
@@ -6,6 +6,15 @@ namespace Engine.Promotions
 {
     public class ComboOffer : IPromotion
     {
+        public IEnumerable<char> ComboSku { get; private set; }
+        public decimal FlatPrice { get; private set; }
+
+        public ComboOffer(IEnumerable<char> comboSku, decimal flatAmount)
+        {
+            ComboSku = comboSku;
+            FlatPrice = flatAmount;
+        }
+
         public Cart Calculate(Cart cart)
         {
             throw new NotImplementedException();
