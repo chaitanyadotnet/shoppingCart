67693926ab5edbcc9b6bbc42d98e1e718e592544
Chaitanya <chaitanya.net2020@gmail.com>
2020-08-05 10:25:56 +0530

    adding basic domain logic

---
+++ PromotionEngine/Engine/Promotions/BuyMoreSaveMore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Promotions
+{
+    public class BuyMoreSaveMore : IPromotion
+    {
+        public Cart Calculate(Cart cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PromotionEngine/Engine/Promotions/IPromotion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Promotions
+{
+    public interface IPromotion
+    {
+
+        Cart Calculate(Cart cart);
+    }
+}
