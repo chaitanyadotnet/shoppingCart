fca067e9bc934e503325e2bb5750e599e39889b1
Chaitanya <chaitanya.net2020@gmail.com>
2020-08-05 12:01:27 +0530

    addressing future requirements

--- PromotionEngine/Engine/Promotions/Discount.cs
+++ PromotionEngine/Engine/Promotions/Discount.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Domain.Models;
+using System;
 
 namespace Engine.Promotions
 {
     public class Discount : IPromotion
     {
+        
         public Cart Calculate(Cart cart)
         {
+            //this is a future class to use for % discounts 
             throw new NotImplementedException();
         }
     }
